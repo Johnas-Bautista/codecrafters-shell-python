a9518465064ea9af772ecae634814ab1e46f3a3b
johnas <johnas@DESKTOP-BEGAUQM>
2026-01-18 13:42:52 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,13 +5,33 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_input = input().split()
         
-        if command == "exit":
-            break
+        if not user_input:
+            continue
+            
+        command = user_input[0]
+        args = user_input[1:]
         
-        print(f"{command}: command not found")
+        result = commands(command, *args)
+        if result is False: 
+            print(f"{command}: command not found")
 
 
+
+def commands(command, *args):
+    COMMANDS = {
+        "exit": lambda code=0, *args: sys.exit(int(code)),
+        "echo": lambda *x: print(" ".join(x)),
+    }
+    
+    if command in COMMANDS:
+        COMMANDS[command](*args)
+        return True
+    else:
+        return False
+        
+    
+    
 if __name__ == "__main__":
     main()
